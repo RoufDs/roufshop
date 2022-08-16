5356e52fa3b2f1d81d7eb0dba494ef909e3e301b
RoufDs <four.3687zero@gmail.com>
2022-08-16 23:53:06 +0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Header from './componemts/Header'
+import Footer from './componemts/Footer'
+
+import HomeScreen from './screen/HomeScreen';
+import ProductScreen from './screen/ProductScreen'
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
